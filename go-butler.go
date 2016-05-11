3800992c6d8e9d5b39a568e39ffd9e6f4c484d8d
Nicholas Dart <nicholasjdart@gmail.com>
2016-05-11 22:45:22 +0100

    added simple gumble app

--- go-butler.go
+++ go-butler.go
@@ -4,11 +4,18 @@ import (
   "fmt"
   "github.com/layeh/gumble/gumble"
   "github.com/layeh/gumble/gumbleutil"
+  "crypto/tls"
+  "net"
 )
 
 func main() {
   config := gumble.NewConfig()
   config.Username = "gumble-test"
+
+  var tlsConfig tls.Config
+  tlsConfig.InsecureSkipVerify = true
+
+  keepAlive := make(chan bool)
 
   config.Attach(gumbleutil.Listener{
     TextMessage: func(e *gumble.TextMessageEvent) {
@@ -16,9 +23,27 @@ func main() {
     },
   })
 
-  _, err := gumble.Dial("server.com:64738", config)
+  config.Attach(gumbleutil.Listener{
+    UserChange: func(e *gumble.UserChangeEvent) {
+      if e.Type.Has(gumble.UserChangeConnected) {
+        e.User.Send("Welcome to the server, " + e.User.Name + "!")
+      }
+    },
+    Disconnect: func(e *gumble.DisconnectEvent) {
+      keepAlive <- true
+    },
+  })
+
+  client, err := gumble.DialWithDialer(new(net.Dialer), "server.com:64738", config, &tlsConfig)
   if err != nil {
     fmt.Println(err)
     panic(err)
   }
+
+  for _, user := range client.Users {
+      fmt.Println(user)
+  }
+
+  <-keepAlive
+
 }
