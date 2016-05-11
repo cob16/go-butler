6932b25b729e3dc4de887931ca2562059d9691d9
Nicholas Dart <nicholasjdart@gmail.com>
2016-05-11 21:37:14 +0100

    Added example gumble config + dial

--- go-butler.go
+++ go-butler.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+  "fmt"
+  "github.com/layeh/gumble/gumble"
+  "github.com/layeh/gumble/gumbleutil"
+)
+
 func main() {
+  config := gumble.NewConfig()
+  config.Username = "gumble-test"
+
+  config.Attach(gumbleutil.Listener{
+    TextMessage: func(e *gumble.TextMessageEvent) {
+        fmt.Printf("Received text message: %s\n", e.Message)
+    },
+  })
+
+  _, err := gumble.Dial("server.com:64738", config)
+  if err != nil {
+    fmt.Println(err)
+    panic(err)
+  }
 }
