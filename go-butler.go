e152a6634fd61d6c789dc5b98afc5322ca03f3fa
cob16 <cob16@aber.ac.uk>
2016-05-11 22:59:08 +0100

    simplelfy + comment

--- go-butler.go
+++ go-butler.go
@@ -1,49 +1,47 @@
 package main
 
 import (
-  "fmt"
-  "github.com/layeh/gumble/gumble"
-  "github.com/layeh/gumble/gumbleutil"
-  "crypto/tls"
-  "net"
+	"fmt"
+	"github.com/layeh/gumble/gumble"
+	"github.com/layeh/gumble/gumbleutil"
+	"crypto/tls"
+	"net"
 )
 
 func main() {
-  config := gumble.NewConfig()
-  config.Username = "gumble-test"
+	config := gumble.NewConfig()
+	config.Username = "better-gumble-test"
 
-  var tlsConfig tls.Config
-  tlsConfig.InsecureSkipVerify = true
+	var tlsConfig tls.Config
+	tlsConfig.InsecureSkipVerify = true
 
-  keepAlive := make(chan bool)
+	keepAlive := make(chan bool)
 
-  config.Attach(gumbleutil.Listener{
-    TextMessage: func(e *gumble.TextMessageEvent) {
-        fmt.Printf("Received text message: %s\n", e.Message)
-    },
-  })
+	config.Attach(gumbleutil.Listener{
+		UserChange: func(e *gumble.UserChangeEvent) {
+			if e.Type.Has(gumble.UserChangeConnected) {
+				e.User.Send("Welcome to the server, " + e.User.Name + "!")
+			}
+		},
+		TextMessage: func(e *gumble.TextMessageEvent) {
+			fmt.Printf("Received text message: %s\n", e.Message)
+		},
+		//kill the program if we are disconnected
+		Disconnect: func(e *gumble.DisconnectEvent) {
+			keepAlive <- true
+		},
+	})
 
-  config.Attach(gumbleutil.Listener{
-    UserChange: func(e *gumble.UserChangeEvent) {
-      if e.Type.Has(gumble.UserChangeConnected) {
-        e.User.Send("Welcome to the server, " + e.User.Name + "!")
-      }
-    },
-    Disconnect: func(e *gumble.DisconnectEvent) {
-      keepAlive <- true
-    },
-  })
+	client, err := gumble.DialWithDialer(new(net.Dialer), "example.com:64738", config, &tlsConfig)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
 
-  client, err := gumble.DialWithDialer(new(net.Dialer), "server.com:64738", config, &tlsConfig)
-  if err != nil {
-    fmt.Println(err)
-    panic(err)
-  }
+	for _, user := range client.Users {
+		fmt.Println(user)
+	}
 
-  for _, user := range client.Users {
-      fmt.Println(user)
-  }
-
-  <-keepAlive
+	<-keepAlive
 
 }
